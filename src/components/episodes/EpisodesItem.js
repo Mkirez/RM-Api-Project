d0a1a69e0105d3def20dd3f77c1672a6cde8bcd1
Mkirez <m.kirez@hotmail.com>
2021-01-20 22:35:26 +0100

    Episode recht getrokken

--- src/components/episodes/EpisodesItem.js
+++ src/components/episodes/EpisodesItem.js
@@ -2,28 +2,23 @@ import React from 'react'
 
 const EpisodesItem = ({ item }) => {
     return (
-        <div className='container'>
-            <div className='row'>
-                <div className='card'>
-                    <div className='cardInner'>
-                        <div className='cardFront'>
-                            <h1>{item.name}</h1>
-                                <ul>
-                                    <li>
-                                    <strong>Air Date:</strong> {item.air_date}
-                                    </li>
-                                    <li>
-                                    <strong>Episode:</strong> {item.episode}
-                                    </li>
-                                    <li id="charachters">
-                                    <strong>Charachters:</strong>{item.characters.pop()}  
-                                    </li>
-                                    
-                                </ul>
-                        </div>
-                    </div>
+        <div className='card'>
+            <div className='cardInner'>
+                <div className='cardFront'>
+                    <h1>{item.name}</h1>
+                        <ul>
+                            <li>
+                            <strong>Air Date:</strong> {item.air_date}
+                            </li>
+                            <li>
+                            <strong>Episode:</strong> {item.episode}
+                            </li>
+                            <li id="charachters">
+                            <strong>Charachters:</strong>{item.characters.pop()}  
+                            </li>
+                        </ul>
                 </div>
-            </div>    
+            </div>
         </div>
       )
     }
